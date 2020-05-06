c15876f57ebafdebf793697633856ab11598f823
spavani <sreeramakumar.pavani@igt.com>
2020-05-06 14:15:59 -0400

    add web controllers and very basic Thymeleaf templates to display authors and books

---
+++ src/main/java/com/learn/spring5web/controllers/AuthorController.java
@@ -0,0 +1,22 @@
+package com.learn.spring5web.controllers;
+
+import com.learn.spring5web.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", this.authorRepository.findAll());
+        return "authors/list";
+    }
+}
